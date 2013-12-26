291dc1235c433161ac3e2dc7df14e5c209f485fe
arn354 <arn354@email.de>
2013-12-26 14:53:36 +0100

    UPDATE MyMetrixMOD 0.9.8a

--- MyMetrixHD_MOD/lib/enigma2/python/Components/Renderer/MetrixMODXPicon.py
+++ MyMetrixHD_MOD/lib/enigma2/python/Components/Renderer/MetrixMODXPicon.py
@@ -1,106 +1,28 @@
-import os
+##
+## Picon renderer by Gruffy .. some speedups by Ghost
+## XPicon mod by iMaxxx
+##
 from Renderer import Renderer
 from enigma import ePixmap
-from Tools.Alternatives import GetWithAlternative
-from Tools.Directories import pathExists, SCOPE_SKIN_IMAGE, SCOPE_CURRENT_SKIN, resolveFilename
-from Components.Harddisk import harddiskmanager
-
-searchPaths = []
-lastPiconPath = None
-
-def initPiconPaths():
-	global searchPaths
-	searchPaths = []
-	for mp in ('/usr/share/enigma2/', '/'):
-		onMountpointAdded(mp)
-	for part in harddiskmanager.getMountedPartitions():
-		onMountpointAdded(part.mountpoint)
-
-def onMountpointAdded(mountpoint):
-	global searchPaths
-	try:
-		path = os.path.join(mountpoint, 'picon') + '/'
-		if os.path.isdir(path) and path not in searchPaths:
-			for fn in os.listdir(path):
-				if fn.endswith('.png'):
-					print "[Picon] adding path:", path
-					searchPaths.append(path)
-					break
-	except Exception, ex:
-		print "[Picon] Failed to investigate %s:" % mountpoint, ex
-
-def onMountpointRemoved(mountpoint):
-	global searchPaths
-	path = os.path.join(mountpoint, 'picon') + '/'
-	try:
-		searchPaths.remove(path)
-		print "[Picon] removed path:", path
-	except:
-		pass
-
-def onPartitionChange(why, part):
-	if why == 'add':
-		onMountpointAdded(part.mountpoint)
-	elif why == 'remove':
-		onMountpointRemoved(part.mountpoint)
-
-def findPicon(serviceName):
-	global lastPiconPath
-	if lastPiconPath is not None:
-		pngname = lastPiconPath + serviceName + ".png"
-		if pathExists(pngname):
-			return pngname
-	global searchPaths
-	for path in searchPaths:
-		if pathExists(path):
-			pngname = path + serviceName + ".png"
-			if pathExists(pngname):
-				lastPiconPath = path
-				return pngname
-	return ""
-
-def getPiconName(serviceName):
-	#remove the path and name fields, and replace ':' by '_'
-	sname = '_'.join(GetWithAlternative(serviceName).split(':', 10)[:10])
-	pngname = findPicon(sname)
-	if not pngname:
-		fields = sname.split('_', 3)
-		if len(fields) > 2 and fields[2] != '2':
-			#fallback to 1 for tv services with nonstandard servicetypes
-			fields[2] = '1'
-			pngname = findPicon('_'.join(fields))
-	return pngname
+from enigma import iServiceInformation, iPlayableService, iPlayableServicePtr
+from Tools.Directories import fileExists, SCOPE_SKIN_IMAGE, SCOPE_CURRENT_SKIN, resolveFilename
 
 class MetrixMODXPicon(Renderer):
+	searchPaths = ('/media/hdd/XPicons/%s/','/media/img/XPicons/%s/','/media/usb/XPicons/%s/','/usr/share/enigma2/XPicons/%s/','/usr/share/enigma2/%s/', '/media/usb/%s/')
+
 	def __init__(self):
 		Renderer.__init__(self)
+		self.path = "picon"
+		self.nameCache = { }
 		self.pngname = ""
-		self.lastPath = None
-		pngname = findPicon("picon_default")
-		self.defaultpngname = None
-		if not pngname:
-			tmp = resolveFilename(SCOPE_CURRENT_SKIN, "picon_default.png")
-			if pathExists(tmp):
-				pngname = tmp
-			else:
-				pngname = resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/picon_default.png")
-		if os.path.getsize(pngname):
-			self.defaultpngname = pngname
-
-	def addPath(self, value):
-		if pathExists(value):
-			global searchPaths
-			if not value.endswith('/'):
-				value += '/'
-			if value not in searchPaths:
-				searchPaths.append(value)
 
 	def applySkin(self, desktop, parent):
-		attribs = self.skinAttributes[:]
+		attribs = [ ]
 		for (attrib, value) in self.skinAttributes:
 			if attrib == "path":
-				self.addPath(value)
-				attribs.remove((attrib,value))
+				self.path = value
+			else:
+				attribs.append((attrib,value))
 		self.skinAttributes = attribs
 		return Renderer.applySkin(self, desktop, parent)
 
@@ -110,17 +32,34 @@ class MetrixMODXPicon(Renderer):
 		if self.instance:
 			pngname = ""
 			if what[0] != self.CHANGED_CLEAR:
-				pngname = getPiconName(self.source.text)
-			if not pngname: # no picon for service found
-				pngname = self.defaultpngname
+				
+				sname = self.source.text
+				pos = sname.rfind(':')
+				if pos != -1:
+					sname = sname[:pos].rstrip(':').replace(':','_')
+				pngname = self.nameCache.get(sname, "")
+				if pngname == "":
+					pngname = self.findPicon(sname)
+					if pngname != "":
+						self.nameCache[sname] = pngname
+			if pngname == "": # no picon for service found
+				pngname = self.nameCache.get("default", "")
+				if pngname == "": # no default yet in cache..
+					pngname = self.findPicon("picon_default")
+					if pngname == "":
+						tmp = resolveFilename(SCOPE_CURRENT_SKIN, "picon_default.png")
+						if fileExists(tmp):
+							pngname = tmp
+						else:
+							pngname = resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/picon_default.png")
+					self.nameCache["default"] = pngname
 			if self.pngname != pngname:
-				if pngname:
-					self.instance.setScale(1)
-					self.instance.setPixmapFromFile(pngname)
-					self.instance.show()
-				else:
-					self.instance.hide()
+				self.instance.setPixmapFromFile(pngname)
 				self.pngname = pngname
 
-harddiskmanager.on_partition_list_change.append(onPartitionChange)
-initPiconPaths()
+	def findPicon(self, serviceName):
+		for path in self.searchPaths:
+			pngname = (path % self.path) + serviceName + ".png"
+			if fileExists(pngname):
+				return pngname
+		return ""
